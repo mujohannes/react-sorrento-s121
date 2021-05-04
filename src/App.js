536b9846ed0fad1199ea2abdbf9afff8f9974661
johannes <johannes.muljana@tafensw.edu.au>
2021-05-04 10:20:27 +1000

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import logo from './logo.svg';
+import Logo from './igloo-solid.png'
 import './App.css';
-import {Text} from './components/Text'
-import {Button} from './components/Button'
-import {List} from './components/List'
 
-const items = [
-  {"name" : "banana" , "colour" : "yellow"},
-  {"name" : "apple" , "colour" : "green"},
-  {"name" : "tomato" , "colour" : "red"}
+import {Header} from './components/Header'
+
+const NavItems = [
+  {"name" : "Home" , "link" : "/home"},
+  {"name" : "The Apartment" , "link" : "/apartment"},
+  {"name" : "Pricing" , "link" : "/pricing"},
+  {"name" : "Book" , "link" : "/book"},
+  {"name" : "Local Area" , "link" : "/local"},
+  {"name" : "Contact" , "link" : "/contact"}
 ]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <Text name="Barry" color="red" />
-        <Text name="Larry" color="yellow" />
-        <Text name="Jane" color="orange" />
-        <Button start={3} />
-        <Button start={100} />
-        <List items={items} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="website">
+      <Header logo={Logo} nav={NavItems} />
+      <main className="content"></main>
+      <footer className="footer"></footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export function Header ( props ) {
+    return (
+        <header className="header">
+            <img className="logo" src={props.logo} />
+            <nav className="navigation">
+                
+            </nav>
+        </header>
+    )
+}
+
+export default Header
